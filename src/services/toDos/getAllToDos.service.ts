f1aacc2d0c1959f6918244b41b5d4b0710565fb4
yancarlodev <yancarlodc@gmail.com>
2022-12-08 16:13:05 -0300

    fix: change logic at get all to-dos to only reatrive owned to-dos

--- src/services/toDos/getAllToDos.service.ts
+++ src/services/toDos/getAllToDos.service.ts
@@ -1,8 +1,12 @@
 import { ToDos } from '@prisma/client'
 import { prisma } from '../../app'
 
-export default async function getAllToDosService(): Promise<ToDos[]> {
-    const allToDos: ToDos[] = await prisma.toDos.findMany()
+export default async function getAllToDosService(userId: string): Promise<ToDos[]> {
+    const allToDos: ToDos[] = await prisma.toDos.findMany({
+        where: {
+            userId
+        }
+    })
 
     return allToDos
 }
